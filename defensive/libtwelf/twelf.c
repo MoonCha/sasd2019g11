a3134014b8abdfaa9d469614a642fed91e0482ce
MoonCall <moonchang@postech.ac.kr>
2020-01-09 16:23:03 +0000

    libtwelf_write: apply sh_addralign to offset again with overflow check

--- defensive/libtwelf/twelf.c
+++ defensive/libtwelf/twelf.c
@@ -1095,6 +1095,17 @@ int libtwelf_write(struct LibtwelfFile *twelf, char *dest_file)
           return_value = ERR_IO;
           goto fail;
         }
+        long alignment = (twelf_section->internal->sh_addralign - (cur_position % twelf_section->internal->sh_addralign)) % twelf_section->internal->sh_addralign;
+        if (__builtin_add_overflow(cur_position, alignment, &cur_position)) {
+          log_info("section_offset exceeds long after alignment");
+          return_value = ERR_IO;
+          goto fail;
+        }
+        if (fseek(outfile, cur_position, SEEK_SET)) {
+          log_info("fseek error");
+          return_value = ERR_IO;
+          goto fail;
+        }
         section_offset = cur_position;
         log_info("non-associated section(index: %lu) offset recalculated: %lu -> %lu", i, twelf_section->internal->sh_offset, section_offset);
         if (twelf_section->type != SHT_NOBITS) {
